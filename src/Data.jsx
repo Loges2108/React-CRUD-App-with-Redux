5848d72deb2fe61242faee8d7714cf693b3a3418
logeswaran.tamtree <logeswaran.tamtree@gmail.com>
2025-08-25 21:45:20 +0530

    second commit

---
+++ src/Data.jsx
@@ -0,0 +1,16 @@
+export const userList = [
+    {
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        id: 1
+    },
+    {
+        name: "Jane Smith",
+        email: "janesmith@gmail.com",
+        id: 2
+    },
+    {   name: "Alice Johnson",
+        email: "alice@gmail.com",
+        id: 3
+    }
+]
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import { configureStore } from '@reduxjs/toolkit'
+import UserReducer from './UserReducer.jsx'
+
+const store = configureStore({
+  reducer: {
+  users:UserReducer 
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
